f2fcde492e9f8400bd17bfa1539be3aab5dfa05e
Timur Nizamov <abc@nizamovtimur.ru>
2024-10-06 00:31:22 +0500

    fix var scopes

--- api/main.py
+++ api/main.py
@@ -1,4 +1,3 @@
-import logging
 from os import environ
 import pathlib
 
@@ -17,7 +16,9 @@ import torch
 load_dotenv("../.env")
 app = FastAPI()
 device = "cuda" if torch.cuda.is_available() else "cpu"
-embeddings = HuggingFaceEmbeddings(model_name="saved_models/USER-bge-m3", device=device)
+embeddings = HuggingFaceEmbeddings(
+    model_name="saved_models/USER-bge-m3"
+)  # let cpu have fun
 audio_model = WhisperModel(
     "saved_models/faster-whisper-medium",
     compute_type="int8",
@@ -35,6 +36,9 @@ retriever = None
 
 @app.post("/index_media")
 async def index_media(media: UploadFile):
+    global vectorstore
+    global retriever
+
     pathlib.Path("data").mkdir(parents=True, exist_ok=True)
     with open(f"data/{media.filename}", "wb") as local_file:
         local_file.write(media.file.read())
@@ -53,26 +57,25 @@ async def index_media(media: UploadFile):
     documents = [
         Document(page_content=" ".join(segment.text for segment in segments_n))
     ]
-    logging.warning(documents[0].page_content)
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=200, add_start_index=True
+        chunk_size=2000, chunk_overlap=300, add_start_index=True
     )
     all_splits = text_splitter.split_documents(documents)
+    # TODO: set video title in `collection_name` for more accurate RAG in cases when more than 1 video were indexed
     vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
     retriever = vectorstore.as_retriever(
-        search_type="similarity", search_kwargs={"k": 3}
+        search_type="similarity", search_kwargs={"k": 5}
     )
 
     return "Retriever is ready. Use /answer"
 
 
 @app.post("/answer")
-async def answer(question: str):
+async def answer_question(question: str):
     if vectorstore is None or retriever is None:
         raise HTTPException(400, detail="Retriever is not ready. Use /index_media")
 
     retrieved_docs = retriever.invoke(question)
-    logging.warning(retrieved_docs)
     messages = [
         SystemMessage(
             content=f"""Действуйте как помощник для ответов на вопросы.
